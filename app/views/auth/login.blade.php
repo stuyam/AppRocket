5cc8f74569f3f59ab6ce7b8ce90bfdb0bcc303c4
Stuart Yamartino <stuart@yamartino.com>
2014-10-16 23:34:22 -0700

    Fixed login styles

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -5,9 +5,14 @@
 @stop
 
 @section('content')
+
+<header>
+  <img id="logo" src="/img/logo-white.png" />
+  <h1>AppRocket</h1>
+</header>
 <div id="box">
   {{ Form::open(['route' => 'post.login']) }}
-    <h1>Login</h1>
+    <h2>Login</h2>
     @include('auth._form')
 
     <div class="form-group">
