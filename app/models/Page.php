dc65c3ab8e3f5628f3a2b30dc83a0f126d99441b
Stuart Yamartino <stuart@yamartino.com>
2014-11-13 00:54:06 -0800

    Updated model to match new table fillable

--- app/models/Page.php
+++ app/models/Page.php
@@ -1,5 +1,5 @@
 <?php
 
 class Page extends \Eloquent {
-	protected $fillable = ['title', 'about', 'image', 'background', 'store_url', 'phone_color', 'text_color'];
+	protected $fillable = ['user_id', 'name', 'data'];
 }
