eca72e0da637bccabf8ca8c30337beb042414b1f
Stuart Yamartino <stuart@yamartino.com>
2014-11-12 18:19:46 -0800

    Dump data to make sure it works

---
+++ app/views/dump.blade.php
@@ -0,0 +1,13 @@
+Title: {{{ $data['title'] }}} <br>
+
+About: {{{ $data['about'] }}} <br>
+
+Store URL: {{{ $data['store_url'] }}} <br>
+
+Copyright: {{{ $data['copyright'] }}} <br>
+
+Phone Color: {{{ $data['phone_color'] }}} <br>
+
+Text Color: {{{ $data['text_color'] }}} <br>
+
+Text Color: {{{ $data['text_color'] }}} <br>
