3f21e28815d78c4504de649643f77cdbf39c2696
Stuart Yamartino <stuart@yamartino.com>
2014-11-14 01:59:32 -0800

    Updated placeholder

--- app/views/edit/app_store.blade.php
+++ app/views/edit/app_store.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
     {{ Form::label('app_store', 'App Store URL') }}<br />
     <span class="error-display">{{$errors->first('app_store')}}</span>
-    {{ Form::text('app_store', $data['app_store'],['placeholder'=>'http://apple.com/23423412412', 'class'=>'form-control']) }}
+    {{ Form::text('app_store', $data['app_store'], ['placeholder'=>'http://apple.com/1234', 'class'=>'form-control']) }}
 </div>
