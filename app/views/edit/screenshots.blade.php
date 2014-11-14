0ab6f2031a38651a410d3a770981cdc5b09aa2dd
Stuart Yamartino <stuart@yamartino.com>
2014-11-14 01:59:13 -0800

    Renamed images to screenshots

--- app/views/edit/screenshots.blade.php
+++ app/views/edit/screenshots.blade.php
@@ -3,18 +3,18 @@
     <span class="error-display">{{$errors->first('screenshot')}}</span>
     <div class="btn-group btn-group-justified" id="top-row">
         <span class="btn btn-default btn-file" id="image-btn">
-            Choose File 1&#42;{{ Form::file('image', ['required'=>'required', 'id'=>'image']) }}
+            Choose File 1&#42;{{ Form::file('screenshot1', ['required'=>'required', 'id'=>'image']) }}
         </span>
         <span class="btn btn-default btn-file" id="image2-btn">
-            Choose File 2{{ Form::file('image2', ['id'=>'image2']) }}
+            Choose File 2{{ Form::file('screenshot2', ['id'=>'image2']) }}
         </span>
     </div>
     <div class="btn-group btn-group-justified" id="bottom-row">
         <span class="btn btn-default btn-file" id="image3-btn">
-            Choose File 3{{ Form::file('image3', ['id'=>'image3']) }}
+            Choose File 3{{ Form::file('screenshot3', ['id'=>'image3']) }}
         </span>
         <span class="btn btn-default btn-file" id="image4-btn">
-            Choose File 4{{ Form::file('image4', ['id'=>'image4']) }}
+            Choose File 4{{ Form::file('screenshot4', ['id'=>'image4']) }}
         </span>
     </div>
 </div>
