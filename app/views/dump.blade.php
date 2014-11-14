18cacedabedf35c38195b79c312064994037d8da
Stuart Yamartino <stuart@yamartino.com>
2014-11-14 01:59:55 -0800

    Dumping all data from the $data object

--- app/views/dump.blade.php
+++ app/views/dump.blade.php
@@ -1,13 +1,14 @@
-Title: {{{ $data['title'] }}} <br>
+<?php dd($data); ?>
+{{--Title: {{{ $data['title'] }}} <br>--}}
 
-About: {{{ $data['about'] }}} <br>
+{{--About: {{{ $data['about'] }}} <br>--}}
 
-Store URL: {{{ $data['store_url'] }}} <br>
+{{--Store URL: {{{ $data['app_store'] }}} <br>--}}
 
-Copyright: {{{ $data['copyright'] }}} <br>
+{{--Copyright: {{{ $data['copyright'] }}} <br>--}}
 
-Phone Color: {{{ $data['phone_color'] }}} <br>
+{{--Phone Color: {{{ $data['phone_color'] }}} <br>--}}
 
-Text Color: {{{ $data['text_color'] }}} <br>
+{{--Text Color: {{{ $data['text_color'] }}} <br>--}}
 
-Text Color: {{{ $data['text_color'] }}} <br>
+{{--Text Color: {{{ $data['text_color'] }}} <br>--}}
