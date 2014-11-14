aa01508e9bc87218f4dde4e2b8e8c197c1b1cdac
Stuart Yamartino <stuart@yamartino.com>
2014-11-14 02:00:49 -0800

    Lots of overall cleanup here and refactoring 24/7

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -1,6 +1,20 @@
 <?php
 
+use AppRocket\FileHandler;
+use AppRocket\Validate;
+
 class PageController extends \BaseController {
+
+  protected $fileHandler;
+  protected $validate;
+  protected $page;
+
+  public function __construct(FileHandler $fileHandler, Validate $validate, Page $page)
+  {
+    $this->fileHandler = $fileHandler;
+    $this->validate = $validate;
+    $this->page = $page;
+  }
 
   public function edit()
   {
@@ -20,42 +34,38 @@ class PageController extends \BaseController {
 
   public function editPost()
   {
-    $userid = Auth::id();
-    $all_post = Input::all();
-    $id = Input::has('id') ? Input::get('id') : false;
+    $user_id          = Auth::id();
+    $post             = Input::all();
+    $id               = Input::has('id') ? Input::get('id') : false;
+    $background_image = Input::file('background_image');
+
     //Check to make sure the person owns the page they are trying to edit
-    if( ! Page::where('user_id', $userid)->where('id', $id)->first() && $id != false)
-    {
-      return "You don't have permission to change this App Rocket Page";
-    }
-    $validator = Validator::make($all_post,
-      [
-        'name'              => "required|min:3|max:255|unique:pages,name,$id|alpha_dash",
-        'title'             => 'required',
-        'about'             => 'required',
-        'text_color'        => 'required',
-//        'screenshot'        => 'required|image',
-        'background_image'  => 'image',
-        'app_store'         => 'url',
-      ]
-    );
+    if( $this->page->canEditName($user_id, $id) )
+      return "You don't have permission to modify this App Rocket Page";
 
-    if ($validator->fails())
-    {
-        return Redirect::back()->withInput()->withErrors($validator);
-    }
+    $this->validate->edit($post, $id);
 
-    $background = $this->getBackground($all_post, Input::file('background'));
+    $background = $this->getBackground($post, $background_image);
 
-    $images = ['image', 'image2', 'image3', 'image4'];
+    $images = ['screenshot1', 'screenshot2', 'screenshot3', 'screenshot4'];
     foreach($images as $i) {
       if(Input::hasFile($i))
-        $screens[] = $this->getScreens(Input::file($i));
+        $screens[] = $this->fileHandler->nameFile(Input::file($i));
     }
 
-    $this->savePage($id, $userid, $all_post, $background, $images);
+    $page_id = $this->page->savePage($id, $user_id, $post, $background, $screens);
 
-    return Redirect::to("/$all_post[name]");
+    if( ! $this->isFirstCharacterHex($background) )
+      $this->fileHandler->saveFile($background_image, $page_id, $background);
+
+    $j = 0;
+    foreach($images as $i) {
+      if(Input::hasFile($i))
+         $this->fileHandler->saveFile(Input::file($i), $page_id, $screens[$j]);
+      $j++;
+    }
+
+    return Redirect::to("/$post[name]");
   }
 
   public function view($name){
@@ -77,48 +87,20 @@ class PageController extends \BaseController {
 
   private function getBackground($input, $image)
   {
-    if($input['back_option'] === 'color')
-      if(substr($input['background_color'], 0, 1) === '#')
+    if($input['back_option'] == 'color'){
+      if($this->isFirstCharacterHex($input['background_color']))
         return $input['background_color'];
       else
         return '#'.$input['background_color'];
+    }
     else
     {
-      $background = md5(uniqid('backgroundID', true)).'.'.$image->getClientOriginalExtension();
-      $image->move(public_path().'/backgrounds', $background);
+      return $this->fileHandler->nameFile($image);
     }
   }
 
-  private function getScreens($image)
+  private function isFirstCharacterHex($val)
   {
-    $img = md5(uniqid('imageID', true)).$image->getClientOriginalExtension();
-    $image->move(public_path().'/screens', $img);
-    return $img;
-
-  }
-
-  private function savePage($id, $userid, $data_input, $background, $images){
-    $data = array_intersect_key($data_input, array_flip([
-      'title',
-      'about',
-      'store_url',
-      'copyright',
-      'phone_color',
-      'text_color',
-    ]));
-
-    $data['background'] = $background;
-    $data['images']     = $images;
-
-    if( $id ){
-      $page = Page::find($id);
-    }
-    else {
-      $page = new Page;
-      $page->user_id = $userid;
-    }
-    $page->name = strtolower($data_input['name']);
-    $page->data = json_encode($data);
-    $page->save();
+    return substr($val, 0, 1) === '#';
   }
 }
