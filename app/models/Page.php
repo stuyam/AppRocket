0072abdcb826c647af7db93298bbe1990e48ebed
Stuart Yamartino <stuart@yamartino.com>
2014-11-15 17:13:16 -0800

    Renaming and reordering of screenshot structure

--- app/models/Page.php
+++ app/models/Page.php
@@ -8,7 +8,7 @@ class Page extends \Eloquent {
     return ! Page::where('user_id', $user_id)->where('id', $id)->first() && $id != false;
   }
 
-  public function savePage($id, $userid, $data_input, $background, $images){
+  public function savePage($id, $user_id, $data_input, $background, $screens){
     $data = array_intersect_key($data_input, array_flip([
       'title',
       'about',
@@ -19,14 +19,14 @@ class Page extends \Eloquent {
     ]));
 
     $data['background']  = $background;
-    $data['screenshots'] = $images;
+    $data = array_merge($data, $screens);
 
     if( $id ){
       $page = Page::find($id);
     }
     else {
       $page = new Page;
-      $page->user_id = $userid;
+      $page->user_id = $user_id;
     }
     $page->name = strtolower($data_input['name']);
     $page->data = json_encode($data);
