93febc47f20e7f20537b81cd20f67f514b6fb5bb
Stuart Yamartino <stuart@yamartino.com>
2014-11-15 17:14:03 -0800

    Adding hidden inputs for images to handle old

--- app/views/edit/screenshots.blade.php
+++ app/views/edit/screenshots.blade.php
@@ -3,18 +3,22 @@
     <span class="error-display">{{$errors->first('screenshot')}}</span>
     <div class="btn-group btn-group-justified" id="top-row">
         <span class="btn btn-default btn-file" id="image-btn">
-            Choose File 1&#42;{{ Form::file('screenshot1', ['required'=>'required', 'id'=>'image']) }}
+            Choose File 1&#42;{{ Form::file('screen-0', ['required'=>'required', 'id'=>'screen-0']) }}
+            {{ Form::hidden('screen-0-meta', $data['screen-0-meta']) }}
         </span>
         <span class="btn btn-default btn-file" id="image2-btn">
-            Choose File 2{{ Form::file('screenshot2', ['id'=>'image2']) }}
+            Choose File 2{{ Form::file('screen-1', ['id'=>'screen-1']) }}
+            {{ Form::hidden('screen-1-meta', $data['screen-1-meta']) }}
         </span>
     </div>
     <div class="btn-group btn-group-justified" id="bottom-row">
         <span class="btn btn-default btn-file" id="image3-btn">
-            Choose File 3{{ Form::file('screenshot3', ['id'=>'image3']) }}
+            Choose File 3{{ Form::file('screen-2', ['id'=>'screen-2']) }}
+            {{ Form::hidden('screen-2-meta', $data['screen-2-meta']) }}
         </span>
         <span class="btn btn-default btn-file" id="image4-btn">
-            Choose File 4{{ Form::file('screenshot4', ['id'=>'image4']) }}
+            Choose File 4{{ Form::file('screen-3', ['id'=>'screen-3']) }}
+            {{ Form::hidden('screen-3-meta', $data['screen-3-meta']) }}
         </span>
     </div>
 </div>
