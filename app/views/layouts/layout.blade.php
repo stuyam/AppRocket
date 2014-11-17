4eaed81ce10d36ea4522f823f0f7af7092f138f4
Stuart Yamartino <stuart@yamartino.com>
2014-11-17 01:01:29 -0800

    Updated bootstrap to the newest version

--- app/views/layouts/layout.blade.php
+++ app/views/layouts/layout.blade.php
@@ -5,7 +5,7 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     @yield('css')
 </head>
@@ -13,7 +13,7 @@
 
 @yield('content')
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
 @yield('js')
 
